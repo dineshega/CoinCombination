82f73b8eb36941f15840d91198ebbd0432f78612
“dineshega” <dinesh15.ega@gmail.com>
2020-10-10 19:40:39 +0530

    Added: Winning Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,8 +2,11 @@
 
 echo "Welcome to Flip Coin Combination"
 
+declare -A singlet
+declare -A doublet
 declare -A triplet
-
+declare -A doub
+declare -A trip
 h=0
 t=0
 read -p "how many times should coin flip: " flip
@@ -17,6 +20,74 @@ function coinFlip()
 			echo "T"
 		fi
 }
+
+for((i=0; i<$flip; i++))
+do
+	singlet[$i]="$( coinFlip )"
+	if [[ "${singlet[$i]}" == "H" ]]
+	then
+		h=$((h+1))
+		hs=$( awk 'BEGIN {print ('$h'/'$flip')*100}')
+	else
+		t=$((t+1))
+		ts=$( awk 'BEGIN {print ('$t'/'$flip')*100}')
+	fi
+done
+echo ${singlet[@]}
+echo "singlet head count: " $h
+echo "singlet tail count: " $t
+echo "head percentage: " $hs
+echo "tail percentage: " $ts
+if [[ $h -gt $t ]]
+then
+	echo "head won"
+elif [ $h -lt $t ]
+then
+	echo "tail won"
+else
+	echo "both won"
+fi
+
+for ((j=0; j<$flip ;j++))
+do
+	doublet[$j]="$( coinFlip )""$( coinFlip )"
+	if [[ "${doublet[$j]}" == "HH" ]]
+	then
+		hh=$((hh+1))
+		hhd=$( awk 'BEGIN {print ('$hh'/'$flip')*100}')
+	elif [[ "${doublet[$j]}" == "TT" ]]
+	then
+		tt=$((tt+1))
+		ttd=$( awk 'BEGIN {print ('$tt'/'$flip')*100}')
+	elif [[ "${doublet[$j]}" == "HT" ]]
+	then
+		ht=$((ht+1))
+		htd=$( awk 'BEGIN {print ('$ht'/'$flip')*100}')
+	else
+		th=$((th+1))
+		thd=$( awk 'BEGIN {print ('$th'/'$flip')*100}')
+	fi
+done
+echo ${doublet[@]}
+
+echo "doublet hh count: " $hh
+echo "doublet tt count: " $tt
+echo "doublet ht count: " $ht
+echo "doublet th count: " $th
+echo "hh percentage: " $hhd
+echo "tt percentage: " $ttd
+echo "ht percentage: " $htd
+echo "th percentage: " $thd
+greatest=0
+for m in $hh $tt $ht $th
+do
+	if [[ $greatest -lt $m ]]
+	then
+		greatest=$m
+	fi
+done
+echo "Winning combo" $greatest
+
 for ((k=0; k<$flip ;k++))
 do
 	triplet[$k]="$( coinFlip )""$( coinFlip )""$( coinFlip )"
@@ -71,3 +142,16 @@ echo "triplet thh precentage: " $thhtri
 echo "triplet tht precentage: " $thttri
 echo "triplet tth precentage: " $tthtri
 echo "triplet ttt precentage: " $ttttri
+
+greatest1=0
+
+for n in $hhh $hht $hth $htt $thh $tht $tth $ttt
+do
+	if [[ $greatest1 -lt $n ]]
+	then
+		greatest1=$n
+	fi
+done
+echo "Winning combo" $greatest1
+
+
