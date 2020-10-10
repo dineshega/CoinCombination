94027a136eda71012a2d6ffa57179001ce124af9
“dineshega” <dinesh15.ega@gmail.com>
2020-10-10 19:36:06 +0530

    Added: Doublet

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,7 +2,7 @@
 
 echo "Welcome to Flip Coin Combination"
 
-declare -A singlet
+declare -A doublet
 
 h=0
 t=0
@@ -18,20 +18,33 @@ function coinFlip()
 		fi
 }
 
-for((i=0; i<$flip; i++))
+for ((j=0; j<$flip ;j++))
 do
-	singlet[$i]="$( coinFlip )"
-	if [[ "${singlet[$i]}" == "H" ]]
+	doublet[$j]="$( coinFlip )""$( coinFlip )"
+	if [[ "${doublet[$j]}" == "HH" ]]
 	then
-		h=$((h+1))
-		hs=$( awk 'BEGIN {print ('$h'/'$flip')*100}')
+		hh=$((hh+1))
+		hhd=$( awk 'BEGIN {print ('$hh'/'$flip')*100}')
+	elif [[ "${doublet[$j]}" == "TT" ]]
+	then
+		tt=$((tt+1))
+		ttd=$( awk 'BEGIN {print ('$tt'/'$flip')*100}')
+	elif [[ "${doublet[$j]}" == "HT" ]]
+	then
+		ht=$((ht+1))
+		htd=$( awk 'BEGIN {print ('$ht'/'$flip')*100}')
 	else
-		t=$((t+1))
-		ts=$( awk 'BEGIN {print ('$t'/'$flip')*100}')
+		th=$((th+1))
+		thd=$( awk 'BEGIN {print ('$th'/'$flip')*100}')
 	fi
 done
-echo ${singlet[@]}
-echo "singlet head count: " $h
-echo "singlet tail count: " $t
-echo "head percentage: " $hs
-echo "tail percentage: " $ts
+echo ${doublet[@]}
+
+echo "doublet hh count: " $hh
+echo "doublet tt count: " $tt
+echo "doublet ht count: " $ht
+echo "doublet th count: " $th
+echo "hh percentage: " $hhd
+echo "tt percentage: " $ttd
+echo "ht percentage: " $htd
+echo "th percentage: " $thd
